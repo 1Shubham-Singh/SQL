54b69d647b68604cffafa9cd6bc97905c9c2fa35
1Shubham-Singh <115469540+1Shubham-Singh@users.noreply.github.com>
2026-01-29 23:37:27 +0530

    Add files via upload

---
+++ AGGREGATE.sql
@@ -0,0 +1,15 @@
+USE shubhamdb;
+--AGGREGATE FUNCTIONS ARE:-
+select avg(salary) from employee;
+select count(*) from employee;
+select count(distinct salary) from employee;
+select sum(salary) from employee;
+select max(salary) from employee;
+select min(salary) from employee;
+select * from employee order by salary desc;
+
+SELECT * FROM employee;
+
+
+
+
